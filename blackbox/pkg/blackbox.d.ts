78217c40b9466e1753d55b8c73ef76c3bbe0a06a
anatoliyzhang <95625793+anatoliyzhang@users.noreply.github.com>
2022-07-25 14:13:17 +0800

    Add files via upload

---
+++ blackbox/pkg/blackbox.d.ts
@@ -0,0 +1,45 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} plaintext
+* @param {string} raw_key
+* @returns {string}
+*/
+export function encrypto(plaintext: string, raw_key: string): string;
+/**
+* @param {string} cipheredtext
+* @param {string} raw_key
+* @returns {string}
+*/
+export function decrypto(cipheredtext: string, raw_key: string): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly encrypto: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly decrypto: (a: number, b: number, c: number, d: number, e: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* Synchronously compiles the given `bytes` and instantiates the WebAssembly module.
+*
+* @param {BufferSource} bytes
+*
+* @returns {InitOutput}
+*/
+export function initSync(bytes: BufferSource): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
